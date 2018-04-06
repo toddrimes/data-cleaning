ade98cf091f89c6b02bfd495041d04a91f2f7c1d
Todd Rimes <todd@rimesmedia.com>
2018-04-05 20:36:39 -0700

    first

---
+++ run_analysis.R
@@ -0,0 +1,48 @@
+# setwd("~/Desktop/coursera/DataCleaning/Week4/CodeProject")
+
+run_analysis <- function() {
+
+        # DOWNLOAD THE SOURCE DATA ZIP FILE
+        download.file("https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip","./getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip")
+        zipF<-"./getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip" %>%
+                unzip(exdir=".")
+        
+        # ROW LABELS AND FEATURE NAMES
+        rowLabels <- read.table("./UCI HAR Dataset/activity_labels.txt")
+        rowLabels[,2] <- as.character(rowLabels[,2])
+        featureNames <- read.table("./UCI HAR Dataset/features.txt")
+        featureNames[,2] <- as.character(featureNames[,2])
+        
+        # KEEP ONLY DATA ON MEAN AND SD
+        extractFeatures <- grep(".*mean.*|.*std.*", featureNames[,2])
+        extractFeatures.names <- featureNames[extractFeatures,2]
+        extractFeatures.names <- gsub('-()', '', extractFeatures.names)
+        
+        # GET THE TEST DATA
+        testData <- read.table("./UCI HAR Dataset/test/X_test.txt")[extractFeatures]
+        testActivityData <- read.table("./UCI HAR Dataset/test/Y_test.txt")
+        testSubjectData <- read.table("./UCI HAR Dataset/test/subject_test.txt")
+        # re-assemble the testing Data
+        testData <- cbind(testSubjectData, testActivityData, testData)
+        colnames(testData) <- c("subject", "activity", extractFeatures.names)
+        
+        # GET THE TRAINING DATA
+        trainData <- read.table("./UCI HAR Dataset/train/X_train.txt")[extractFeatures]
+        trainActivityData <- read.table("./UCI HAR Dataset/train/Y_train.txt")
+        trainSubjectData <- read.table("./UCI HAR Dataset/train/subject_train.txt")
+        # re-assemble the training Data
+        trainData <- cbind(trainSubjectData, trainActivityData, trainData)
+        colnames(trainData) <- c("subject", "activity", extractFeatures.names)
+        
+        # merge datasets and add labels
+        combinedData <- rbind(testData, trainData)
+        
+        # turn activities & subjects into factors
+        combinedData$activity <- factor(combinedData$activity, levels = rowLabels[,1], labels = rowLabels[,2])
+        combinedData$subject <- as.factor(combinedData$subject)
+        
+        cdl <- melt(combinedData, id = c("subject", "activity"))
+        cdw <- dcast(cdl, subject+activity ~ variable, mean)
+        print(getwd());
+        write.table(cdw, "tidy.txt", row.names = FALSE, quote = FALSE)
+}
